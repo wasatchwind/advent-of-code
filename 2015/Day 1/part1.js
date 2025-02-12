d85eb73e77cb7cce2e8029ebdab7ca0a877854d6
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-11 17:23:39 -0700

    Update part1.js

--- 2015/Day 1/part1.js
+++ 2015/Day 1/part1.js
@@ -8,11 +8,13 @@ file.addEventListener('change', (event) => {
   reader.onload = (load) => {
     const puzzleInput = load.target.result
 
+    // -----------------------------------------------------------------------------------------
     // Day 1 Part 1: 138 (https://adventofcode.com/2015/day/1)
     let floor = 0
     for (const char of puzzleInput) {
       floor = char === '(' ? floor += 1 : floor -= 1
     }
     console.log(`Santa's target floor is:`, floor)
+    // -----------------------------------------------------------------------------------------
   }
 })
