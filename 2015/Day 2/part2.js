c6947d4f53f613f91ba483651853f1e026d1d814
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-12 15:30:27 -0700

    Update part2.js

--- 2015/Day 2/part2.js
+++ 2015/Day 2/part2.js
@@ -1,1 +1,29 @@
+file.addEventListener('change', (event) => {
+  const fileText = event.target.files[0]
+  const reader = new FileReader()
+  reader.readAsText(fileText)
+  reader.onload = (load) => {
+    const puzzleInput = load.target.result
 
+    // -----------------------------------------------------------------------------------------
+    // Day 2 Part 2: 3842356 (https://adventofcode.com/2015)
+    const presentDimensions = puzzleInput.split('\n')
+    let totalsqft = 0
+
+    for (const currentDimension of presentDimensions) {
+
+      // Use regular expression to find Length, Width, and Height
+      const [L, W, H] = currentDimension.match(/\d{1,2}/g).map(Number)
+      
+      // Set smallest to largest via sort() but only keep the first 2 (omit the largest)
+      const [smallest, secondSmallest] = [L, W, H].sort((a, b) => a - b)
+      
+      const ribbon = 2 * (smallest + secondSmallest)
+      const bow = L * W * H
+      
+      totalsqft += ribbon + bow
+    }
+    console.log('Total feet of ribbon needed:', totalsqft)
+    // -----------------------------------------------------------------------------------------
+  }
+})
