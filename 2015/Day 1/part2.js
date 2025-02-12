9a1746e0ea848766fae5d0b76ae927720841bc4e
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-11 17:39:49 -0700

    Update part2.js

--- 2015/Day 1/part2.js
+++ 2015/Day 1/part2.js
@@ -1,1 +1,25 @@
-// add
+'use strict'
+const file = document.getElementById('input')
+
+file.addEventListener('change', (event) => {
+  const fileText = event.target.files[0]
+  const reader = new FileReader()
+  reader.readAsText(fileText)
+  reader.onload = (load) => {
+    const puzzleInput = load.target.result
+
+    // -----------------------------------------------------------------------------------------
+    // Day 1 Part 2: 1771 (https://adventofcode.com/2015/day/1)
+    let floor = 0
+    
+    for (let charIndex = 0; charIndex < puzzleInput.length; charIndex++) {
+      floor = puzzleInput[charIndex] === '(' ? floor += 1 : floor -= 1
+      
+      if (floor === -1) {
+        console.log('The character position that causes Santa to enter the basement is', charIndex + 1)
+        break
+      }
+    }
+    // -----------------------------------------------------------------------------------------
+  }
+})
