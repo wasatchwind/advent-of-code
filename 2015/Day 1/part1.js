bb971114859cb37d811c2f0f13441e54b4df3259
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-11 17:22:05 -0700

    Update part1.js

--- 2015/Day 1/part1.js
+++ 2015/Day 1/part1.js
@@ -1,1 +1,18 @@
-// put solution here
+'use strict'
+const file = document.getElementById('input')
+
+file.addEventListener('change', (event) => {
+  const fileText = event.target.files[0]
+  const reader = new FileReader()
+  reader.readAsText(fileText)
+  reader.onload = (load) => {
+    const puzzleInput = load.target.result
+
+    // Day 1 Part 1: 138 (https://adventofcode.com/2015/day/1)
+    let floor = 0
+    for (const char of puzzleInput) {
+      floor = char === '(' ? floor += 1 : floor -= 1
+    }
+    console.log(`Santa's target floor is:`, floor)
+  }
+})
