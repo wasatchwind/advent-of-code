199cbe107c7324dab87a2df5e7330cd3edc9a5a2
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-12 16:13:24 -0700

    Update part2.js

--- 2015/Day 2/part2.js
+++ 2015/Day 2/part2.js
@@ -1,29 +1,26 @@
-file.addEventListener('change', (event) => {
-  const fileText = event.target.files[0]
-  const reader = new FileReader()
-  reader.readAsText(fileText)
-  reader.onload = (load) => {
-    const puzzleInput = load.target.result
+'use strict'
+// Day 2 Part 2 answer: 3842356
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
 
-    // -----------------------------------------------------------------------------------------
-    // Day 2 Part 2: 3842356 (https://adventofcode.com/2015)
-    const presentDimensions = puzzleInput.split('\n')
-    let totalsqft = 0
+  // Split text into individual lines, each line the length, width, and height of a present
+  const presentDimensions = puzzleInput.split('\n')
+  let totalsqft = 0
 
-    for (const currentDimension of presentDimensions) {
+  // Loop through each present
+  for (const currentDimension of presentDimensions) {
 
-      // Use regular expression to find Length, Width, and Height
-      const [L, W, H] = currentDimension.match(/\d{1,2}/g).map(Number)
-      
-      // Set smallest to largest via sort() but only keep the first 2 (omit the largest)
-      const [smallest, secondSmallest] = [L, W, H].sort((a, b) => a - b)
-      
-      const ribbon = 2 * (smallest + secondSmallest)
-      const bow = L * W * H
-      
-      totalsqft += ribbon + bow
-    }
-    console.log('Total feet of ribbon needed:', totalsqft)
-    // -----------------------------------------------------------------------------------------
+    // Use regular expression to extract length, width, and height and convert string to number
+    const [L, W, H] = currentDimension.match(/\d{1,2}/g).map(Number)
+
+    // Order dimensions smallest to largest via sort() but omit the largest
+    const [smallest, secondSmallest] = [L, W, H].sort((a, b) => a - b)
+
+    const ribbon = 2 * (smallest + secondSmallest)
+    const bow = L * W * H
+
+    totalsqft += ribbon + bow
   }
+  console.log('Total feet of ribbon needed:', totalsqft)
+  document.getElementById('answer').innerText = totalsqft
 })
