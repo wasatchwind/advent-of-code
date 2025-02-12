4ba8d4d5b322715900e4807706e91078465971a9
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-12 16:25:47 -0700

    Update part2.js

--- 2015/Day 1/part2.js
+++ 2015/Day 1/part2.js
@@ -1,28 +1,21 @@
 'use strict'
-const file = document.getElementById('input')
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
 
-file.addEventListener('change', (event) => {
-  const fileText = event.target.files[0]
-  const reader = new FileReader()
-  reader.readAsText(fileText)
-  reader.onload = (load) => {
-    const puzzleInput = load.target.result
+  // Day 1 Part 2 answer: 1771
+  let floor = 0
 
-    // -----------------------------------------------------------------------------------------
-    // Day 1 Part 2: 1771 (https://adventofcode.com/2015)
-    let floor = 0
+  // Loop through each character of the input string by index
+  for (let charIndex = 0; charIndex < puzzleInput.length; charIndex++) {
+    
+    // Only 2 options: '(' to go up one floor or ')' to go down one floor
+    floor = puzzleInput[charIndex] === '(' ? floor += 1 : floor -= 1
 
-    // Itereate over each character of the input string by index
-    for (let charIndex = 0; charIndex < puzzleInput.length; charIndex++) {
-      // Only options are either '(' to go up one floor or ')' to go down one floor
-      floor = puzzleInput[charIndex] === '(' ? floor += 1 : floor -= 1
-
-      // When floor is -1 Santa has gone into the basement
-      if (floor === -1) {
-        console.log('The character position that causes Santa to enter the basement is', charIndex + 1)
-        break
-      }
+    // When floor is -1 Santa has gone into the basement
+    if (floor === -1) {
+      console.log('The character position that causes Santa to enter the basement is', charIndex + 1)
+      document.getElementById('answer').innerText = charIndex + 1
+      break
     }
-    // -----------------------------------------------------------------------------------------
   }
 })
