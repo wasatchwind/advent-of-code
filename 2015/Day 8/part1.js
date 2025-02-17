745b85ec5b1c44533ba81cdddac2506e826680b9
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-17 14:16:49 -0700

    Update part1.js

--- 2015/Day 8/part1.js
+++ 2015/Day 8/part1.js
@@ -5,7 +5,7 @@ document.addEventListener('puzzleInputLoaded', () => {
   // Day 8 Part 1 answer: 1371
 
   // Split text into individual lines, each line a string to be evaluated
-  const strings = puzzleInput.split('\n').map(item => item.trim())
+  const strings = puzzleInput.split('\n').map(line => line.trim())
   let originalLength = 0
   let inMemoryLength = 0
 
