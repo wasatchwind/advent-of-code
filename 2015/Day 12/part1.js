c8e1d70b6ede4b14b815ead63d821b285b882f2c
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-17 18:43:29 -0700

    Update part1.js

--- 2015/Day 12/part1.js
+++ 2015/Day 12/part1.js
@@ -1,1 +1,22 @@
+'use strict'
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
 
+  // Day 12 Part 1 answer: 191164
+
+  const json = JSON.parse(puzzleInput)
+  let total = 0
+  traverseObject(json)
+
+  function traverseObject(json) {
+    for (const value of Object.values(json)) {
+      if (typeof value === 'number') total += value
+
+      // If value exists and is an object (includes arrays), recurse
+      if (value && typeof value === 'object') traverseObject(value)
+    }
+  }
+
+  document.getElementById('answer').innerText = total
+  console.log('The sum of all the numbers is', total)
+})
