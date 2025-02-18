1730ec492e3c069ecde3d9165494c9cfe76413e7
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-18 15:48:30 -0700

    Update part1.js

--- 2015/Day 17/part1.js
+++ 2015/Day 17/part1.js
@@ -1,1 +1,34 @@
+'use strict'
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
 
+  // Day 17 Part 1 answer: 4372
+
+  // Process input text line by line into a flat array of numbers
+  const containers = puzzleInput.split('\n')
+    .map(line => line.trim())
+    .filter(line => line !== '')
+    .map(Number)
+
+  const targetCapacity = 150
+  const combinations = []
+  allCombos(0, targetCapacity, [])
+
+  // Recursive function to generate all valid combinations of containers
+  function allCombos(index, targetCapacity, currentCombo) {
+
+    // If target capacity is reached
+    if (targetCapacity === 0) {
+      combinations.push(currentCombo)
+      return
+    }
+
+    // Recursion
+    for (let i = index; i < containers.length; i++) {
+      allCombos(i + 1, targetCapacity - containers[i], [...currentCombo, containers[i]])
+    }
+  }
+
+  document.getElementById('answer').innerText = combinations.length
+  console.log('The total number of different combinations of containers is', combinations.length)
+})
