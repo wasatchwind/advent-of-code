5b21d7ed84f179fe7f162ad22bd400be6220178b
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-02-20 09:20:22 -0700

    Update part1.js

--- 2015/Day 25/part1.js
+++ 2015/Day 25/part1.js
@@ -1,1 +1,39 @@
+'use strict'
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
 
+  // Day 25 Part 1 answer: 2650453
+
+  // Parse puzzle input into row and column numbers
+  const input = puzzleInput.match(/\d{4}/g).map(item => Number(item))
+  const codeCoords = { 'row': input[0], 'col': input[1] }
+  
+  // Since the sequence order uses Cantor's diagonal arguement, a formula can be used
+  // to find the position (order number, not code itself) at row 1 and the target column (3083): 4753986
+  const orderNumAtRow1TargetCol = (codeCoords.col * (codeCoords.col + 1)) / 2
+
+  // Knowing the order number at row 1, col 3083, now can traverse down the column with a formula
+  // to find the position (order number, not code itself) at target row, col (2978, 3083): 18361853
+  const orderNumAtTarget = orderNumAtRow1TargetCol + (codeCoords.row - 1) * codeCoords.col + (codeCoords.row - 1) * (codeCoords.row - 2) / 2
+  
+  const firstCode = 20151125 // Given
+  const multiplier = 252533 // Given
+  const divisor = 33554393 // Given
+
+  // Calculate the code at the coordinates using the position/order number
+  const code = (firstCode * modularExponentiation(multiplier, orderNumAtTarget - 1, divisor)) % divisor
+
+  // Function that uses modular exponentiation (exponentiation by squaring) for maximum efficiency
+  function modularExponentiation(base, exponent, modulus) {
+    let code = 1
+    while (exponent > 0) {
+      if (exponent % 2 === 1) code = (code * base) % modulus
+      base = (base * base) % modulus
+      exponent = Math.floor(exponent / 2)
+    }
+    return code
+  }
+
+  document.getElementById('answer').innerText = code
+  console.log('The code to give the machine is', code)
+})
