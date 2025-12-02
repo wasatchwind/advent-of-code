5f31777db1c059e650c5fe23b88db752088198bb
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-12-02 09:59:38 -0700

    Update part1.js

--- 2025/Day 02/part1.js
+++ 2025/Day 02/part1.js
@@ -1,1 +1,34 @@
-//
+'use strict';
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
+
+  // Day 2 Part 1 answer: 23560874270
+
+  // Parse input into usable data
+  const idRanges = puzzleInput
+    .trim()
+    .split(',')
+    .map(line => {
+      const [start, end] = line.split('-').map(Number);
+      return { start, end };
+    });
+
+  let sum = 0;
+
+  for (const { start, end } of idRanges) {
+    for (let i = start; i <= end; i++) {
+      if (checkValidity(i)) sum += i;
+    }
+  }
+
+  function checkValidity(num) {
+    const string = String(num); // Convert to string to compare digits
+    const length = string.length;
+    if (length % 2 !== 0) return false; // Only even number of digits can be invalid
+    const half = length / 2;
+    return string.slice(0, half) === string.slice(half); // If invalid, return it to be counted
+  }
+
+  document.getElementById("answer").innerText = sum;
+  console.log("The sum of all invalid IDs is", sum);
+});
