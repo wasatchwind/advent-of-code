599f31c5483f7efd7f2735f9785f6e369df4542d
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-12-02 12:46:36 -0700

    Update part2.js

--- 2025/Day 02/part2.js
+++ 2025/Day 02/part2.js
@@ -1,1 +1,42 @@
-//
+'use strict';
+// Use event listener to execute JS after puzzle input is received via text file
+document.addEventListener('puzzleInputLoaded', () => {
+
+  // Day 2 Part 2 answer: 44143124633
+
+  // Parse input into usable data
+  const idRanges = puzzleInput
+    .trim()
+    .split(',')
+    .map(line => {
+      const [start, end] = line.split('-').map(Number);
+      return { start, end };
+    });
+
+  let sum = 0;
+
+  for (const { start, end } of idRanges) {
+    for (let i = start; i <= end; i++) {
+      if (checkValidity(i)) sum += i;
+    }
+  }
+
+  function checkValidity(num) {
+    const string = String(num); // Convert to string to compare digits
+    const length = string.length;
+
+    // Loop through all possible evenly divisible segments and count the value if they all match
+    for (let segment = length - 1; segment >= 1; segment--) {
+      if (length % segment === 0) {
+        const options = [];
+        for (let i = 0; i < length; i += segment) {
+          options.push(string.slice(i, i + segment));
+        }
+        if (options.every(value => value === options[0])) return Number(string)
+      }
+    }
+  }
+
+  document.getElementById("answer").innerText = sum;
+  console.log("The sum of all invalid IDs is", sum);
+});
