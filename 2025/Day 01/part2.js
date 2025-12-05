8954df54de1e5465bab86b4df25464fa88acf40c
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-12-05 08:24:07 -0700

    Update part2.js

--- 2025/Day 01/part2.js
+++ 2025/Day 01/part2.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 // Use event listener to execute JS after puzzle input is received via text file
-document.addEventListener('puzzleInputLoaded', () => {
+document.addEventListener("puzzleInputLoaded", () => {
 
   // Day 1 Part 2 answer: 6671
 
